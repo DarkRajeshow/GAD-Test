c76b0884504bdbf77c5b8ecf73a23d2670cc8241
DarkRajeshow <rajannaadeli@gmail.com>
2024-11-28 19:01:35 +0530

    removed cors

--- app.js
+++ app.js
@@ -12,11 +12,6 @@ const app = express();
 const __dirname = path.resolve();
 
 
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true
-}));
-
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
